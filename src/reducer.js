39df948b8f798912d553169aa8b8b31ad88e4b87
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-04-26 19:27:43 -0400

    Implement remove functionality

--- src/reducer.js
+++ src/reducer.js
@@ -1,10 +1,24 @@
 
-import {DECREASE, INCREASE, CLEAR_CART} from './actions';
+import {DECREASE, INCREASE, CLEAR_CART, REMOVE} from './actions';
 
 function reducer(state, action) {
     if(action.type === CLEAR_CART) {
         return {...state, cart:[]};
     }
+    if(action.type === DECREASE) {
+        
+    }
+    if(action.type === INCREASE) {
+        
+    }
+    if(action.type === REMOVE) {
+       return {
+        ...state,
+        cart:state.cart.filter(cartItem => cartItem.id !==
+        action.payload.id)
+       }
+    }
+
     return state;
   }
 
