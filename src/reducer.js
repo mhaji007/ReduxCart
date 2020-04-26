689ad1434961a78d08bd5eae509bb2c5733b78b4
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-04-26 16:42:52 -0400

    Implement clear cart functionality

--- src/reducer.js
+++ src/reducer.js
@@ -1,8 +1,10 @@
 
-import {DECREASE, INCREASE} from './actions';
+import {DECREASE, INCREASE, CLEAR_CART} from './actions';
 
 function reducer(state, action) {
-
+    if(action.type === CLEAR_CART) {
+        return {...state, cart:[]};
+    }
     return state;
   }
 
