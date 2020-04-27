c68a887e1bef166f3e4ce12bfa9c06162d25414a
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-04-26 22:02:34 -0400

    Implement total number and price functionalities

--- src/reducer.js
+++ src/reducer.js
@@ -1,5 +1,5 @@
 
-import {DECREASE, INCREASE, CLEAR_CART, REMOVE} from './actions';
+import {DECREASE, INCREASE, CLEAR_CART, REMOVE, GET_TOTALS} from './actions';
 
 function reducer(state, action) {
     if(action.type === CLEAR_CART) {
@@ -11,7 +11,7 @@ function reducer(state, action) {
         
         tempCart = state.cart.filter(cartItem => cartItem.id !==
             action.payload.id)
-            
+
         } else {
             tempCart = state.cart.map((cartItem)=>{
                 if(cartItem.id === action.payload.id){
@@ -41,6 +41,29 @@ function reducer(state, action) {
         action.payload.id)
        }
     }
+
+    if (action.type === GET_TOTALS) {
+        let {total, amount} = state.cart.reduce((cartTotal, cartItem)=>{
+
+                const {price, amount} = cartItem;
+                const itemTotal = price * amount;
+
+                cartTotal.total += itemTotal;
+                cartTotal.amount +=amount;
+            
+            return cartTotal;
+        },
+        {
+            total:0,
+            amount:0
+        }
+        
+        );
+
+        total = parseFloat(total.toFixed(2));
+        return {...state, total, amount};
+    };
+
 
     return state;
   }
