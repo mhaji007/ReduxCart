f4f93fa364ad79e8cfbf9b6672e931077e98e035
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-04-26 20:34:14 -0400

    Implement increase funcitonality

--- src/reducer.js
+++ src/reducer.js
@@ -9,7 +9,14 @@ function reducer(state, action) {
         
     }
     if(action.type === INCREASE) {
-        
+        let tempCart = state.cart.map((cartItem)=>{
+            if(cartItem.id === action.payload.id){
+                cartItem = {...cartItem, amount:cartItem.amount+1}
+            }
+            
+            return cartItem
+        })
+        return {...state, cart:tempCart}
     }
     if(action.type === REMOVE) {
        return {
